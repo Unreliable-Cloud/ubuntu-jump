d4218b6ce8a29fabefe3e7a1710cdc6175b1f9fd
Mike B <mike@donthurt.us>
2023-01-02 02:59:10 +0000

    more updates

--- tools/app.py
+++ tools/app.py
@@ -1,12 +1,23 @@
 import dev_pod
 from flask import Flask, request, redirect, session, url_for, render_template, send_from_directory, send_file, Response
 
+namespace = ""
+deploymentName = ""
+
 app = Flask(__name__)
+
 
 @app.route("/", methods=['GET'])
 def root():
-    createPod = dev_pod.deploy_dev_pod("test-pod-0", "testing")
-    return createPod
+    createPod = dev_pod.deploy_dev_pod(namespace, deploymentName)
+    return render_template('parent-root.html',
+                            namespace=namespace,
+                            deploymentName=deploymentName,
+                            createOutput=createPod)
+
+@app.route("/deploy", methods=['GET'])
+def deploy():
+    pass
 
 if __name__ == "__main__":
     app.run()
