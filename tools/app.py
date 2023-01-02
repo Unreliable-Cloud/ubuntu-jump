07b7e0f4072158aa9dbc2996435ab407b23a597b
Mike B <mike@donthurt.us>
2023-01-02 03:53:15 +0000

    trivial

--- tools/app.py
+++ tools/app.py
@@ -19,10 +19,11 @@ def deploy():
     if request.method == "POST":
         namespace = request.form.get("namespace")
         deploymentName = request.form.get("deploymentName")
-        createPod = dev_pod.deploy_dev_pod(namespace, deploymentName)
+        dev_pod.deploy_dev_pod(namespace, deploymentName)
 
         return render_template('parent-deploy.html',
-                                createOutput=createPod)
+                                namespace=namespace,
+                                deploymentName=deploymentName)
 
 if __name__ == "__main__":
     app.run()
