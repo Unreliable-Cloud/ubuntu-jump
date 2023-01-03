f8b09c4ce1dc0e6eb4f5a92d5a824660da4a6b8f
Mike B <mike@donthurt.us>
2023-01-03 21:49:55 +0000

    use parent template

--- app/app.py
+++ app/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
-    return render_template('index.html',
+    return render_template('parent-index.html',
                             namespace=namespace,
                             deploymentName=deploymentName)
 
