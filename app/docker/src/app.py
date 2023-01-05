6a86979bdd61f137bc5465e1b28d7cb054adccba
Michael Beasley <michael.beasley@alvaria.com>
2023-01-05 17:37:48 -0600

    - add option to choose shell - rename tilt directory

--- app/docker/src/app.py
+++ app/docker/src/app.py
@@ -1,19 +1,12 @@
 import dev_pod
 from flask import Flask, request, redirect, url_for, render_template
 
-namespace = ""
-deploymentName = ""
-createOutput = ""
-backupState = ""
-
 app = Flask(__name__)
 
 
 @app.route("/", methods = ["GET"])
 def root():
-    return render_template('parent-index.html',
-                            namespace=namespace,
-                            deploymentName=deploymentName)
+    return render_template('parent-index.html')
 
 @app.route("/deploy", methods = ["GET", "POST"])
 def deploy():
@@ -22,17 +15,26 @@ def deploy():
             "namespace": request.form.get("namespace"),
             "deploymentName": request.form.get("deploymentName"),
             "backupState": request.form.get("backupState"),
+            "shell": request.form.get("shell"),
         }
-        if deployPost['backupState'] is None:
-            deployPost['backupState'] = "false"
+
+        if deployPost['shell'] == None:
+          deployPost['shell'] = "/bin/bash"
         else:
-            deployPost['backupState'] = "true"
-        dev_pod.deploy_dev_pod(deployPost['namespace'], deployPost['deploymentName'], deployPost['backupState'])
+          deployPost['shell'] = "/usr/bin/zsh"
+
+        if deployPost['backupState'] == None:
+            deployPost['backupState'] = False
+        else:
+            deployPost['backupState'] = True
+
+        dev_pod.deploy_dev_pod(deployPost['namespace'], deployPost['deploymentName'], deployPost['backupState'], deployPost['shell'])
 
         return render_template('parent-deploy.html',
                                 namespace=deployPost['namespace'],
                                 deploymentName=deployPost['deploymentName'],
-                                backupState=deployPost['backupState'])
+                                backupState=deployPost['backupState'],
+                                shell=deployPost['shell'])
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8000', debug=True)
